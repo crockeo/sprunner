b64215c3f47028066652720eb3a36f74a037c7ce
Cerek Hillen <cerekh@gmail.com>
2023-03-10 14:39:33 -0800

    add flake stuff

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix.url = "github:nix-community/gomod2nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    gomod2nix,
+    nixpkgs
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            gomod2nix.overlays.default
+          ];
+        };
+      in
+        rec {
+          packages = {
+            depShell = pkgs.mkShell {
+              packages = [
+                pkgs.gomod2nix
+              ];
+            };
+
+            server = pkgs.buildGoApplication {
+              name = "sprunner";
+              modules = ./gomod2nix.toml;
+              src = ./.;
+            };
+          };
+
+          defaultPackage = packages.server;
+        }
+    );
+}
